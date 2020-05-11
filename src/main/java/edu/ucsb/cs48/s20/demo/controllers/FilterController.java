8abb8ed336f4f0b6e65c70ae300836c9627478f3
briankangucsb <briankang@ucsb.edu>
2020-05-10 21:43:59 -0700

    bk modified FilterController.java so that a list of counties now actually shows up

--- src/main/java/edu/ucsb/cs48/s20/demo/controllers/FilterController.java
+++ src/main/java/edu/ucsb/cs48/s20/demo/controllers/FilterController.java
@@ -5,10 +5,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import edu.ucsb.cs48.s20.demo.formbeans.CountyFilter;
+import edu.ucsb.cs48.s20.demo.repositories.CountyRepository;
+
 
 @Controller
 public class FilterController {
+
+    private CountyRepository countyRepository = null;
+
+    @Autowired
+	public void ApplicationController(CountyRepository countyRepository) {
+	       this.countyRepository = countyRepository;
+	}
 
     @GetMapping("/county/search")
 
@@ -20,6 +30,7 @@ public class FilterController {
 
     public String getEarthquakesResults(Model model, CountyFilter countyFilter) {
         model.addAttribute("countyFilter", countyFilter);
+        model.addAttribute("counties", countyRepository.findAll());
         return "counties/results";
     }
 
