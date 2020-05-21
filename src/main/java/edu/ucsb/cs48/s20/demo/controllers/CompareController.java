ac349cba815a3b65da586a0162d06565063870a5
briankangucsb <briankang@ucsb.edu>
2020-05-21 03:03:31 -0700

    bk - created select and results page for comparison features and a link on the header

---
+++ src/main/java/edu/ucsb/cs48/s20/demo/controllers/CompareController.java
@@ -0,0 +1,37 @@
+package edu.ucsb.cs48.s20.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import edu.ucsb.cs48.s20.demo.formbeans.CompareCounty;
+import edu.ucsb.cs48.s20.demo.repositories.CountyRepository;
+
+
+@Controller
+public class CompareController {
+
+    private CountyRepository countyRepository = null;
+
+    @Autowired
+	public void ApplicationController(CountyRepository countyRepository) {
+	       this.countyRepository = countyRepository;
+	}
+
+    @GetMapping("/county/compare-select")
+
+    public String getCountiesComparison(CompareCounty compareCounty) {
+        return "counties/compare-select";
+    }
+
+    @GetMapping("/county/compare-results")
+
+    public String getComparisonResults(Model model, CompareCounty compareCounty) {
+        model.addAttribute("compareCounty", compareCounty);
+
+        return "counties/compare-results";
+    }
+
+}
---
+++ src/main/java/edu/ucsb/cs48/s20/demo/formbeans/CompareCounty.java
@@ -0,0 +1,28 @@
+package edu.ucsb.cs48.s20.demo.formbeans;
+
+public class CompareCounty {
+
+    private String countyone;
+    private String countytwo;
+
+    public CompareCounty() {}
+
+    // getters
+    public String getCountyone() {
+       return countyone;
+    }
+
+    public String getCountytwo() {
+        return countytwo;
+    }
+
+    // setters
+    public void setCountyone(String one) {
+        countyone = one;
+    }
+
+    public void setCountytwo(String two) {
+        countytwo = two;
+    }
+
+}
