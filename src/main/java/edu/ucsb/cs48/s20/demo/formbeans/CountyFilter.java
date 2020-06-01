1ed409a0661257b02c584832aeb06441967d7648
briankangucsb <briankang@ucsb.edu>
2020-06-01 03:36:00 -0700

    bk - made advance filter search and results pages and input methods

--- src/main/java/edu/ucsb/cs48/s20/demo/formbeans/CountyFilter.java
+++ src/main/java/edu/ucsb/cs48/s20/demo/formbeans/CountyFilter.java
@@ -2,15 +2,43 @@ package edu.ucsb.cs48.s20.demo.formbeans;
 
 public class CountyFilter {
 
+    // 1
+    private double minpopulation;
+    private double maxpopulation;
+
+    // 2
     private double minincome;
     private double maxincome;
+    
+    // 3
     private double minhousecost;
     private double maxhousecost;
+    
+    // 4
+    private double mintemperature;
+    private double maxtemperature;
+
+    // 5
+    private double mincrimerate;
+    private double maxcrimerate;
+
+    // 6
+    private double minunemploymentrate;
+    private double maxunemploymentrate;
+
 
     public CountyFilter() {
     }
 
 // getters
+    public double getMinpopulation() {
+        return minpopulation;
+    }
+
+    public double getMaxpopulation() {
+        return maxpopulation;
+    }
+
     public double getMinincome() {
         return minincome;
     }
@@ -27,7 +55,39 @@ public class CountyFilter {
         return maxhousecost;
     }
 
+    public double getMintemperature() {
+        return mintemperature;
+    }
+
+    public double getMaxtemperature() {
+        return maxtemperature;
+    }
+
+    public double getMincrimerate() {
+        return mincrimerate;
+    }
+
+    public double getMaxcrimerate() {
+        return maxcrimerate;
+    }
+
+    public double getMinunemploymentrate() {
+        return minunemploymentrate;
+    }
+
+    public double getMaxunemploymentrate() {
+        return maxunemploymentrate;
+    }
 // setters
+
+    public void setMinpopulation(double minPop) {
+        minpopulation = minPop;
+    }
+
+    public void setMaxpopulation(double maxPop) {
+        maxpopulation = maxPop;
+    }
+
     public void setMinincome(double minPay) {
         minincome = minPay;
     }
@@ -42,6 +102,30 @@ public class CountyFilter {
 
     public void setMaxhousecost(double maxHome) {
         maxhousecost = maxHome;
+    }
+
+    public void setMintemperature(double minTemp) {
+        mintemperature = minTemp;
+    }
+
+    public void setMaxtemperature(double maxTemp) {
+        maxtemperature = maxTemp;
+    }
+
+    public void setMincrimerate(double minCrime) {
+        mincrimerate = minCrime;
+    }
+
+    public void setMaxcrimerate(double maxCrime) {
+        maxcrimerate = maxCrime;
+    }
+
+    public void setMinunemploymentrate(double minUnemploy) {
+        minunemploymentrate = minUnemploy;
+    }
+
+    public void setMaxunemploymentrate(double maxUnemploy) {
+        maxunemploymentrate = maxUnemploy;
     }
 
 }
