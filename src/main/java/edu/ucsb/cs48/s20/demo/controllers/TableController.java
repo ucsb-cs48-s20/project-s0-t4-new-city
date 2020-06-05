f95042d3c2d10031481d797fa79a3678725774c3
Ari Poalkof <a_polakof@ucsb.edu>
2020-06-05 00:57:12 -0300

    main page

--- src/main/java/edu/ucsb/cs48/s20/demo/controllers/TableController.java
+++ src/main/java/edu/ucsb/cs48/s20/demo/controllers/TableController.java
@@ -1,7 +1,11 @@
 package edu.ucsb.cs48.s20.demo.controllers;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,19 +18,26 @@ import edu.ucsb.cs48.s20.demo.repositories.CountyRepository;
 
 
 @Controller
-@RequestMapping("/counties/")
-public class CountyController {
+public class TableController {
+
+    private Logger logger = LoggerFactory.getLogger(MapController.class);
 
     private final CountyRepository countyRepository;
 
+    private String key;
+
     @Autowired
-    public CountyController(CountyRepository countyRepository) {
+    public TableController(CountyRepository countyRepository) {
         this.countyRepository = countyRepository;
     }
 
-    @GetMapping("list")
-    public String showUpdateForm(Model model) {
+    @GetMapping("/table/index")
+    public String showSurveyForm(Model model) {
+        // logger.info("key={}", key);
+        // String googleMapUrl =
+        //     String.format("https://maps.googleapis.com/maps/api/js?key=%s&callback=initMap", key);
+        // model.addAttribute("googleMapsUrl", googleMapUrl);
         model.addAttribute("counties", countyRepository.findAll());
-        return "index";
+        return "table/index";
     }
 }
